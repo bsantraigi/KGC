f644143d91c49fadc9b933857bef2e9047f42796
nisargss97 <nisargss97@gmail.com>
2018-11-24 02:50:17 +0530

    Uploading codes 1(ndkgc)

---
+++ ndkgc/ops/__sampling/CMakeFiles/sampling.dir/DependInfo.cmake
@@ -0,0 +1,21 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/15CS10030/ConMask/ndkgc/ops/__sampling/pairwise_sampler.cpp" "/home/15CS10030/ConMask/ndkgc/ops/__sampling/CMakeFiles/sampling.dir/pairwise_sampler.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/home/15CS10030/anaconda3/envs/tensorflow/lib/python3.5/site-packages/tensorflow/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ ndkgc/ops/__sampling/CMakeFiles/sampling.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/sampling.dir/pairwise_sampler.cpp.o"
+  "libsampling.pdb"
+  "libsampling.so"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/sampling.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
